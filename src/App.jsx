50b395d0c8849362918be9ac28d688a706d05443
Arunalu Bamunusinghe <130811052+ArunaluB@users.noreply.github.com>
2024-01-22 18:48:10 +0530

    complte login and registration

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import Login from './Components/Login/Login'
+import Register from './Components/Register/Register'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Login /> </div>
+  },
+  {
+    path: '/register',
+    element: <div><Register /> </div>
+  }
+
+])
+
+function App() {
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  )
+}
+
+export default App
