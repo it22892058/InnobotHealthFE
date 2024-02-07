acc6e8f9545e2d98fd7815cb03a5e28af09150cf
AvindiObeyesekere <avindiobeyesekere1@gmail.com>
2024-02-07 23:10:55 +0530

    icons added

--- src/Components/common/header/Header.jsx
+++ src/Components/common/header/Header.jsx
@@ -4,12 +4,30 @@ import { Navbar } from '../../common';
 import logo from './logo.png'
 import Search from '../search/Search';
 import './Header.css';
-import { BsArrowLeftShort, BsSearch } from 'react-icons/bs';
-import { AiFillEnvironment } from 'react-icons/ai';
+import { FaInstagram, FaInbox } from "react-icons/fa";
+import { BsArrowLeftShort, BsSearch,BsChevronDown, BsReverseLayoutTextSidebarReverse} from 'react-icons/bs';
+import { AiFillEnvironment, AiOutlineBarChart, AiOutlineFileText, AiOutlineSetting } from 'react-icons/ai';
 import { MdPeople } from "react-icons/md";
+import {RiDashboardFill} from "react-icons/ri";
+import { RxHamburgerMenu } from "react-icons/rx";
 console.log(logo);
 function Header (){
   const[open,setOpen]= useState(true);
+  const[submenuOpen,setSubmenuOpen]= useState(false);
+  const Menus=[
+    {title:"Dashboard"},
+    {title:"People",icon: <MdPeople/>},
+    {title:"Media",spacing:true,icon:<FaInstagram />},
+    {title:"Projects",icon:<BsReverseLayoutTextSidebarReverse/>,
+     submenu:true,
+     submenuItems:[
+      {title:"submenu "},
+     ],
+    },
+    {title: "Pages",icon:<AiOutlineFileText/>},
+    {title: "Inbox",icon:<FaInbox />},
+    {title: "Settings",icon:<AiOutlineSetting/>},
+  ];
     return(
         <section className="header">
            <section className="header-top">
@@ -42,15 +60,46 @@ function Header (){
 />
         
          <div className="inline-flex">
-         <MdPeople className={`bg-amber text-4xl rounded cursor-pointer float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}/>
+         <RxHamburgerMenu  className={`bg-amber text-4xl rounded cursor-pointer float-left mr-2 duration-500 ${open && "rotate-[360deg]"}`}/>
 
          
          </div>
 
-         <div className={`flex items-center rounded-md bg-light-white mt-6 ${!open ? "px-4" : "px-2.5"} py-2`}>
-            <BsSearch className="text-black text-lg block float-left cursor-pointer  mr-5"/>
+         <div className={`flex items-center rounded-md bg-light-white mt-6 ${!open ? "px-2" : "px-4"} py-2`}>
+            <BsSearch className={`text-black text-lg block float-left cursor-pointer  ${open && "mr-5"}`}/>
         <input type={"search"} className={`text-base bg-transparent w-full text-black focus:outline-none $(!open && "hidden")`} placeholder="Enter your search"/>
          </div>
+        <ul className="pt-2">
+          {Menus.map((menu,index)=>(
+              <>
+              <li key={index} className={`text-black text-sm flex items-center gap-x-4 cursor-pointer p-2 hover:bg-light-white rounded-md ${menu.spacing ? "mt-9" : "mt-4"} `}>
+               <span className="text-2xl block float-left">
+                {menu.icon ? menu.icon : <RiDashboardFill/>}
+               </span>
+                <span className={`text-base font-medium flex-1 duration-200 ${!open && "hidden"}`}>
+                  {menu.title}
+                  </span>
+                  {menu.submenu && open && (
+                    <BsChevronDown className={` ${submenuOpen && "rotate-180"}`} onClick={()=>
+                       setSubmenuOpen(!submenuOpen)
+                      }/>
+                  )}
+              </li>
+              {menu.submenu && submenuOpen && open &&(
+                <ul>
+                  {menu.submenuItems.map((submenuItem,index)=>(
+                    <li key={index} className="text-black text-sm flex items-center gap-x-4 cursor-pointer p-2 px-5 hover:bg-light-white rounded-md">
+                      {submenuItem.title}
+                    </li>
+                  )
+                  )}
+                </ul>
+              )}
+              </>
+          ))}
+        </ul>
+
+
           </div>
 
         </div>
